753135e73f05ee5bfbaebe8c26ceeb8dc5631619
Aaron Walker <a.walker@base2services.com>
2016-01-12 09:25:48 +1100

    added support for installing docker and deploying containers

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,10 +3,12 @@
 # Attribute:: default
 #
 # Copyright (C) 2014 base2Services
-# 
+#
 # All rights reserved - Do Not Redistribute
 #
 
 default['system']['timezone'] = "Australia/Melbourne"
 default['common']['packages'] = ['telnet', 'mc', 'screen', 'sysstat','traceroute']
 default['common']['user'] = 'base2'
+
+default['base2']['docker']['images'] = []
