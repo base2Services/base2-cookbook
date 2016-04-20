48a3c2b48ec59a850fb36fdf26c9790327d7fe85
Aaron Walker <a.walker@base2services.com>
2016-04-20 10:45:19 +1000

    fixes windows chef bootstrap plus adds modes for override the cookbook and run list

--- files/default/windows/EC2-Bootstrap.ps1
+++ files/default/windows/EC2-Bootstrap.ps1
@@ -1,12 +1,13 @@
 Param(
-  [string]$RuntimeCookbook = "runtime",
+  [string]$RuntimeCookbook,
+  [string]$OverrideRunList,
   [string]$ChefOverride = "C:\chef\override.json"
 )
 
 function Get-EC2InstanceTag {
   Param(
     [string][Parameter(Mandatory=$True)]$Tag,
-    [string][Parameter(Mandatory=$True)]$InstanceID
+    [string][Parameter(Mandatory=$True)]$InstanceID,
     [string][Parameter(Mandatory=$True)]$Region
   )
 
@@ -20,11 +21,10 @@ function Invoke-Chef {
   Param(
     [string][Parameter(Mandatory=$True)]$Environment,
     [string][Parameter(Mandatory=$True)]$EnvironmentType,
-    [string][Parameter(Mandatory=$True)]$CookbookName,
-    [string][Parameter(Mandatory=$True)]$Role
+    [string][Parameter(Mandatory=$True)]$RunList
   )
 
-  $chef_args = "--local-mode -o recipe['$CookbookName::$role']"
+  $chef_args = "--local-mode -o $RunList"
   if (Test-Path $ChefOverride) {
     $chef_args = "$chef_args -j $ChefOverride"
   }
@@ -39,7 +39,6 @@ function Invoke-Chef {
 
   Write-Output "running chef-client $chef_args"
   cmd.exe /c "C:\opscode\chef\bin\chef-client $chef_args"
-
 }
 
 # Get Instance Details
@@ -50,16 +49,26 @@ $environment = Get-EC2InstanceTag -Tag Environment -InstanceID $instanceId -Regi
 $environmenttype = Get-EC2InstanceTag -Tag EnvironmentType -InstanceID $instanceId -Region $Region
 $role = Get-EC2InstanceTag -Tag Role -InstanceID $instanceId -Region $Region
 
+if($RuntimeCookbook) {
+  $run_list = "recipe['$RuntimeCookbook::$role']"
+} else {
+  $run_list = "recipe['$role']"
+}
+
+if($OverrideRunList) {
+  $run_list = $OverrideRunList
+}
 
 Write-Output "Bootstrap process called..."
 Write-Output "ChefOverride: $ChefOverride"
 Write-Output "RuntimeCookbook: $RuntimeCookbook"
+Write-Output "Role: $role"
 Write-Output "Region: $Region"
 
 #Run Chef
 if($environment -and $role) {
-  Write-Output "Invoking chef for $environment environment, $role role"
-	Invoke-Chef -Environment $environment -EnvironmentType $environmenttype -CookbookName $RuntimeCookbook -Role $role
+  Write-Output "Invoking chef for $environment environment, $run_list role"
+	Invoke-Chef -Environment $environment -EnvironmentType $environmenttype -RunList $run_list
 } else {
 	Write-Output "Failed to bootstrap $instanceId is missing Environment, Role or Name Tags"
 	Exit 1
