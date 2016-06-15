ece46b354a039fbe7622545c7aee6028f385b439
Aaron Walker <a.walker@base2services.com>
2016-06-15 11:38:41 +1000

    fixed codedeploy chefspec

--- spec/codedeploy_spec.rb
+++ spec/codedeploy_spec.rb
@@ -11,6 +11,10 @@ require_relative 'spec_helper'
 describe 'base2::codedeploy' do
   let(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
+  before do
+    stub_command("false").and_return(false)
+  end
+
   it 'does not raise an exception' do
     expect { chef_run }.to_not raise_error
   end
